3e13fc279eb3f3c7e394ff5d5f981784088e1177
Pranav <jawajipranav@gmail.com>
2021-12-04 13:20:05 +0530

    Dark mode

--- components/Layout.js
+++ components/Layout.js
@@ -1,39 +1,89 @@
-import React from "react";
+import React, { useContext } from "react";
 import Head from "next/head";
 import NextLink from "next/link";
-import { AppBar, Container, Link, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Container,
+  Link,
+  Toolbar,
+  Typography,
+  createMuiTheme,
+  ThemeProvider,
+  CssBaseline,
+  Switch,
+} from "@mui/material";
 import useStyles from "../utils/styles";
+import { Store } from "../utils/store";
+import Cookies from "js-cookie";
 
 function Layout({ title, description, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
+  console.log("darkMode", darkMode);
+  const theme = createMuiTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+    },
+    palette: {
+      mode: darkMode ? "dark" : "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  });
   const classes = useStyles();
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
+    const newDarkMode = !darkMode;
+    Cookies.set("darkMode", newDarkMode ? "ON" : "OFF");
+  };
   return (
     <div>
       <Head>
         <title>{title ? `${title} - Next Amazona` : "Next Amazona"}</title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navBar}>
-        <Toolbar>
-          <NextLink href="/" passHref>
-            <Link>
-              <Typography className={classes.brand}>amazona</Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href="/cart" passRef>
-              <Link>Cart</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navBar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>amazona</Typography>
+              </Link>
             </NextLink>
-            <NextLink href="/login" passRef>
-              <Link>Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        <Typography>All rights reserved. Next Amazona.</Typography>
-      </footer>
+            <div className={classes.grow}></div>
+            <div>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+              <NextLink href="/cart" passRef>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passRef>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. Next Amazona.</Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head></Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
         <body>
           <Main />
           <NextScript />
